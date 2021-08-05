c88d333502999b1340792dd1bd195e3ccbad4f09
wissmedia <wissmedia@gmail.com>
2021-08-06 03:42:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,54 @@
-// import dependendcies
-// require('dotenv').config()
+// # import dependendcies
 const express = require('express')
 const path = require('path')
 const logger = require('morgan')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 const createError = require('http-errors');
 
-// import required files
+// # import required files
 const config = require('./config')
 
-// set const
+// # set app const
 const app = express()
 const host = config.app.host
 const port = config.app.port
 
+// # set db const
+const dbHost = config.db.host
+const dbPort = config.db.port
+const dbUser = config.db.user
+const dbPass = config.db.pass
+const dbName = config.db.database
 
-// run express app
-app.listen(port, host, () => {
-  console.log(`App listening at http://${host}:${port}`)
-})
+// # dbString (full string) => 'mongodb://user:pass@host:port/database'
+// const dbURI = config.db.db_string
+
+// # dbString (partial)
+const dbURI = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+
+// # run express app (no db)
+// app.listen(port, host, () => {
+//   console.log(`App listening at http://${host}:${port}`)
+// })
+
+// # run express app (with mongodb)
+/**
+ * Change `Connected to DB at ${dbHost}:${dbPort}`
+ * to
+ * `Connected to DB at ${dbURI}`
+ * if using dbURI full string
+ */
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(result => {
+    console.log(`Connected to DB at ${dbHost}:${dbPort}`)
+    app.listen(port, host, () => {
+      console.log(`App listening at http://${host}:${port}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 // view engine
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +58,7 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser())
 app.use(logger('dev'))
 
 // app routes
@@ -35,12 +67,12 @@ app.get('/', (req, res) => {
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
