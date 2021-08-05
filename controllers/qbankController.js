e72501f8a7430d1549afab4ac26dfbe4386e2bdc
wissmedia <wissmedia@gmail.com>
2021-08-06 04:16:22 +0800

    Create qbankController.js

---
+++ controllers/qbankController.js
@@ -0,0 +1,38 @@
+const qbank_index = (req, res) => {
+  let navTitle = []
+  let menus = []
+  let navMenus = []
+  res.send('qbank - index')
+}
+const qbank_detail = (req, res) => {
+  let navTitle = []
+  let menus = []
+  let navMenus = []
+  res.send('qbank - detail')
+}
+const qbank_add_get = (req, res) => {
+  let navTitle = []
+  let menus = []
+  let navMenus = []
+  res.send('qbank - add_get')
+}
+const qbank_add_post = (req, res) => {
+  let navTitle = []
+  let menus = []
+  let navMenus = []
+  res.send('qbank - add_post')
+}
+const qbank_delete = (req, res) => {
+  let navTitle = []
+  let menus = []
+  let navMenus = []
+  res.send('qbank - delete')
+}
+
+module.exports = {
+  qbank_index,
+  qbank_detail,
+  qbank_add_get,
+  qbank_add_post,
+  qbank_delete
+}
