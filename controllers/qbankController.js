a8c4b9336f6ac4f8b01aaed419da222f5a70c334
wissmedia <wissmedia@gmail.com>
2021-08-07 18:01:15 +0800

    Update qbankController.js

--- controllers/qbankController.js
+++ controllers/qbankController.js
@@ -1,38 +1,43 @@
+let appTitle = "Paperon";
 const qbank_index = (req, res) => {
-  let navTitle = []
-  let menus = []
-  let navMenus = []
-  res.send('qbank - index')
-}
+  let navTitle = [];
+  let menus = [];
+  let navMenus = [];
+  res.render("qbank/qbank", {
+    appTitle,
+    navTitle: "Bank Pertanyaan",
+    navMenus,
+  });
+};
 const qbank_detail = (req, res) => {
-  let navTitle = []
-  let menus = []
-  let navMenus = []
-  res.send('qbank - detail')
-}
+  let navTitle = [];
+  let menus = [];
+  let navMenus = [];
+  res.send("qbank - detail");
+};
 const qbank_add_get = (req, res) => {
-  let navTitle = []
-  let menus = []
-  let navMenus = []
-  res.send('qbank - add_get')
-}
+  let navTitle = [];
+  let menus = [];
+  let navMenus = [];
+  res.send("qbank - add_get");
+};
 const qbank_add_post = (req, res) => {
-  let navTitle = []
-  let menus = []
-  let navMenus = []
-  res.send('qbank - add_post')
-}
+  let navTitle = [];
+  let menus = [];
+  let navMenus = [];
+  res.send("qbank - add_post");
+};
 const qbank_delete = (req, res) => {
-  let navTitle = []
-  let menus = []
-  let navMenus = []
-  res.send('qbank - delete')
-}
+  let navTitle = [];
+  let menus = [];
+  let navMenus = [];
+  res.send("qbank - delete");
+};
 
 module.exports = {
   qbank_index,
   qbank_detail,
   qbank_add_get,
   qbank_add_post,
-  qbank_delete
-}
+  qbank_delete,
+};
